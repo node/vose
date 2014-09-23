5cec5ac099c5640c979c6edf78438a38aede5ff4
node <nodexy@gmail.com>
2014-09-23 18:16:06 +0800

    update readme ,add dist tool

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+ 
+setup (
+    name = 'vose',
+    version = '0.1',
+    py_modules = ['vose'],
+    author = 'Chris Yang',
+    author_email = 'nodexy@gmail.com',
+    description = 'vose alias method for random sampling ',
+    url = 'http://github.com/node/vose'
+)
---
+++ vose.py
@@ -0,0 +1,23 @@
+#!/usr/bin python
+"""
+Vose alias method for random sampling in python 
+
+https://github.com/node/vose
+
+"""
+__author__ = 'Chris Yang'
+__version__ = '0.1'
+
+
+class Vose:
+	pass
+
+
+	def next():
+		pass
+
+
+if __name__ == '__main__':
+	pass
+
+# END
